029cdb9ae5ae2e78aa7a30d27c9620944d6a5d2f
rakshit <rakshitnagaraj951@gmail.com>
2025-03-03 18:12:33 +0530

    first-commit

---
+++ story/src/Component/foodsale/FoodOrder.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import FoodOrder from "./FoodOrder";
+
+export default {
+  title: "Food Ordering UI",
+  component: FoodOrder,
+};
+
+export const Default = () => <FoodOrder />;
